495168af1b1a607b58873c8457dd777939a02d43
SarahWooller <SarahWooller@users.noreply.github.com>
2025-11-05 12:03:54 +0000

    configure vite and minor amendments to FilterApp.jsx

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,12 +1,22 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// Replace 'repo-name' with the actual name of your GitHub repository
 const repoName = 'CRUK_datahub_landing_page'
 
 export default defineConfig({
   plugins: [react()],
-  // 🔑 Crucial step for GitHub Pages
   base: `/${repoName}/`,
+
+  build: {
+    rollupOptions: {
+      input: {
+        // Map a name (key) to the path (value) for each HTML file
+        // The path must be absolute, which 'resolve' helps create.
+        main_vert_bar: resolve(__dirname, 'vert_bar.html'), // Assuming a.html is at the root
+        main_top_bar: resolve(__dirname, 'top_bar.html'), // Assuming b.html is at the root
+      },
+    },
+  },
 })
