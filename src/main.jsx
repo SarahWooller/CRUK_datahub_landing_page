608a9644c5b74ff874adc1f0618ba412e61514e2
SarahWooller <woollersarah@gmail.com>
2025-12-08 12:42:59 +0000

    Improves the filters but needs work before building

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 const links =
     <div>
         <p>
-            <a href="/src/top_bar.html"> Example with the filters given as a horizontal navbar. </a>
+            <a href="./src/top_bar.html"> Example with the filters given as a horizontal navbar. </a>
         </p>
         <p>
-            <a href="/src/vert_bar.html"> Example with the filters given as a vertical navbar. </a>
+            <a href="./src/vert_bar.html"> Example with the filters given as a vertical navbar. </a>
         </p>
         <p>
-            <a href="/src/just_vert_bar.html"> Example with just the filters given as a vertical navbar. </a>
+            <a href="./src/just_vert_bar.html"> Example with just the filters given as a vertical navbar. </a>
         </p>
         <p>
             <a href="./metadata.html"> Example page of how the metadata might be displayed. </a>
@@ -18,7 +18,7 @@ const links =
             <a href="./structural_metadata.html"> Example page of how the structural metadata might be displayed. </a>
         </p>
         <p>
-            <a href="/src/upload.html"> Early mock up so I can explore how to change the guidance and schemas. </a>
+            <a href="./src/upload.html"> Early mock up so I can explore how to change the guidance and schemas. </a>
         </p>
     </div>;
 
