5d607d443d4e091ad06cc8718d8faac423946d98
SarahWooller <SarahWooller@users.noreply.github.com>
2026-01-06 16:04:52 +0000

    improving the upload page

--- python_helper_code_and_jsons/move_refs.py
+++ python_helper_code_and_jsons/move_refs.py
@@ -1,4 +1,4 @@
-"""`npm run build` creates html files in dist with the wrong path to the assets. This function corrects that,
+"""`npm run build` creates html files in dist with the wrong path to the assets2. This function corrects that,
 while I get my head around moving older versions of the pages into vite."""
 
 import os
@@ -8,8 +8,8 @@ def main():
     directory = "vite/dist"
     paths = [f"{directory}/{f}" for f in os.listdir(directory) if f[-4:] == "html"]
     print(paths)
-    incorrect = "CRUK_datahub_landing_page/assets"
-    correct = "CRUK_datahub_landing_page/vite/dist/assets"
+    incorrect = "CRUK_datahub_landing_page/assets2"
+    correct = "CRUK_datahub_landing_page/vite/dist/assets2"
     for path in paths:
         with open(path) as f:
             file = f.read().replace(incorrect, correct)
