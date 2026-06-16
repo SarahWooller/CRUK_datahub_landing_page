156ca32673c327f681ca3ab7999bf1f319d4c62c
SarahWooller <SarahWooller@users.noreply.github.com>
2026-06-16 11:53:22 +0100

    debugging projects

--- src/projects_vite.jsx
+++ src/projects_vite.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Introduction } from './components/ProjectIntroduction.jsx';
 import { ProjectsSection } from './components/ProjectsSection.jsx';
 import { Header } from './components/Header.jsx';
-import { FeedbackWidget } from './components/FeedbackWidget.jsx';
-import { InstructionsWidget } from './components/InstructionsWidget.jsx';
+
 
 function renderReactComponent(targetId, Component) {
   const targetElement = document.getElementById(targetId);
@@ -23,8 +22,3 @@ function renderReactComponent(targetId, Component) {
 renderReactComponent('header', <Header/>);
 renderReactComponent('introduction', <Introduction/>);
 renderReactComponent('projectsSection', <ProjectsSection/>);
-renderReactComponent('feedback_widget', <FeedbackWidget/>);
-renderReactComponent(
-  'instructions_widget',
-  <InstructionsWidget fileUrl="/studies_help.md" />
-);
