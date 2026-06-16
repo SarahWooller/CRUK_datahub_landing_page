47e86c0cec79e2d4fdc6d2e8d8f021621e858b8f
SarahWooller <SarahWooller@users.noreply.github.com>
2026-06-16 12:26:09 +0100

    ensuring qr codes

--- src/projects_vite.jsx
+++ src/projects_vite.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Introduction } from './components/ProjectIntroduction.jsx';
 import { ProjectsSection } from './components/ProjectsSection.jsx';
 import { Header } from './components/Header.jsx';
+import FloatingQRCode from './components/FloatingQRCode.jsx';
 
 
 function renderReactComponent(targetId, Component) {
@@ -22,3 +23,4 @@ function renderReactComponent(targetId, Component) {
 renderReactComponent('header', <Header/>);
 renderReactComponent('introduction', <Introduction/>);
 renderReactComponent('projectsSection', <ProjectsSection/>);
+renderReactComponent('qr', <FloatingQRCode/>);
